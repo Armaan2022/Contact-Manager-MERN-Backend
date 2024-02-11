262c60775b680a918fe85b3a80e9cdac742cff68
Armaan4270 <adas5@ualberta.ca>
2024-02-10 23:12:16 -0700

    changes

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 const connectDB = async() => {
-    return mongoose.connect(process.env.MONGODB_URL)
+    //return mongoose.connect(process.env.MONGODB_URL)
+    return mongoose.connect("mongodb://127.0.0.1/contacts_data")
   .then(() => console.log("Connection to database established..."))
   .catch((err) => console.error("Error connecting to database:", err));
 }; 
